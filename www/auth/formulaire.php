882719b050792045079117d61ac3b0efc90d3c57
jbdwwm48 <jb.charbonnier.dwwm48@gmail.com>
2025-02-20 15:14:56 +0100

    hash & reword nav

--- www/auth/formulaire.php
+++ www/auth/formulaire.php
@@ -13,9 +13,7 @@ if (isset($_SESSION['utilisateur'])) {
     }
 ?>
     <form class="d-flex flex-column flex-md-row flex-wrap justify-content-md-center gap-2 ps-md-5" action="auth/login.php" method="POST">
-        <label for="login">Nom d'utilisateur :</label>
         <input type="text" id="login" name="login" required>
-        <label for="mot_de_passe">Mot de passe :</label>
         <input type="password" id="mot_de_passe" name="mot_de_passe" required>
         <button type="submit">Se connecter</button>
     </form>
--- www/auth/initDb.php
+++ www/auth/initDb.php
@@ -6,9 +6,10 @@ $password = 'greta_refuge';
 $dsn = "mysql:host=$host;port=3306;dbname=$dbname;charset=utf8;allowPublicKeyRetrieval=true";
 
 try {
+
     $pdo = new PDO($dsn, $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connexion réussie !"; // Vous pouvez commenter ou supprimer cette ligne
 } catch (PDOException $e) {
+
     die("Erreur de connexion : " . $e->getMessage());
 }
