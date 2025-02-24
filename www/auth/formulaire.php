f5e490331be6b8de23aad2801a4d2a93702c31aa
jbdwwm48 <jb.charbonnier.dwwm48@gmail.com>
2025-02-24 09:31:57 +0100

    pre merge

--- www/auth/formulaire.php
+++ www/auth/formulaire.php
@@ -8,7 +8,6 @@ if (isset($_SESSION['utilisateur'])) {
 
     echo '<a href="../backoffice/dashboard.php"><button>Dashboard</button></a>';
     echo '<a href="../auth/logout.php"><button>Déconnexion</button></a>';
-
 } else {
     if (isset($_SESSION['error_message'])) {
         echo "<p style='color: red;'>" . $_SESSION['error_message'] . "</p>";
